d5f75cbe6072c8f731d842de9d24718eb3bd82ee
NickPnevmati <nickpnevmati@duck.com>
2024-02-05 18:13:48 +0200

    Changed GUI lib to PyQt6 and now stuff works

--- main.py
+++ main.py
@@ -1,34 +1,11 @@
-# Import the required libraries
-from tkinter import *
-from pystray import MenuItem as item
-import pystray
-from PIL import Image
+from src.MainWindow import MainWindow
+from PyQt6.QtWidgets import QApplication
+import sys
 
-# Create an instance of tkinter frame or window
-win=Tk()
-win.title("System Tray Application")
+def main():
+	app = QApplication(sys.argv)
+	w = MainWindow(app)
+	app.exec()
 
-# Set the size of the window
-win.geometry("700x350")
-
-# Define a function for quit the window
-def quit_window(icon, item):
-   icon.stop()
-   win.destroy()
-
-# Define a function to show the window again
-def show_window(icon, item):
-   icon.stop()
-   win.after(100, win.deiconify())
-
-# Hide the window and show on the system taskbar
-def hide_window():
-   win.withdraw()
-   image=Image.open("Logo512.png")
-   menu=(item(text='Quit', action=quit_window), item(text='Show', action=show_window))
-   icon=pystray.Icon(name="name", icon=image, title="My System Tray Icon", menu=menu)
-   icon.run()
-
-win.protocol('WM_DELETE_WINDOW', hide_window)
-
-win.mainloop()
+if __name__ == '__main__':
+   	main()
