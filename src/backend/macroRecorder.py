695ef10cd3218b22c5a22d9b22eab28fa796e002
NickPnevmati <nickpnevmati@duck.com>
2024-02-07 21:42:12 +0200

    Implemented MacroRecorder class with abstract callback class for handling

--- src/backend/macroRecorder.py
+++ src/backend/macroRecorder.py
@@ -1,22 +1,51 @@
 from pynput import keyboard, mouse
+from abc import ABC, abstractclassmethod
 
-def on_press(key):
-    try:
-        print(f'Alphanumeric key pressed: {key.char}')
-    except AttributeError:
-        print(f'Special key pressed: {key}')
+class AbstractRecorderCallbackHandler(ABC):
+    @abstractclassmethod
+    def handleKeyPress(key, suppress : bool = False) -> None:
+        pass
 
-def on_release(key):
-    print(f'Key released: {key}')
-    if key == keyboard.Key.esc:
-        # Stop listener
-        return False
+    @abstractclassmethod
+    def handleKeyRelease(key, suppress : bool = False) -> None:
+        pass
 
-def on_click(x, y, button, pressed):
-    if pressed:
-        print(f'Mouse clicked at ({x}, {y}) with {button}')
+    @abstractclassmethod
+    def handleMouseMove(x: int, y: int) -> (bool | None):
+        pass
 
-# Collect events until released
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
-    with mouse.Listener(on_click=on_click) as listener:
-        listener.join()
+    @abstractclassmethod
+    def handleMouseClick(x: int, y: int, button, asdf: bool) -> (bool | None):
+        pass
+
+    @abstractclassmethod
+    def handleMouseScroll(x: int, y: int, z: int, w: int) -> (bool | None):
+        pass
+
+class MacroRecorder():
+    def __init__(self, handler: AbstractRecorderCallbackHandler) -> None:
+        self.handler = handler
+        self.running = False
+
+    def start(self, mouseListener: bool = True, keyboardListener: bool = True):
+        if self.running:
+            raise Exception("Recorder is already running, cannot re-instantiate")
+        self.__initListeners()
+        if mouseListener:
+            self.mouseListener.start()
+        if keyboardListener:
+            self.keyboardListener.start()
+        self.running = True
+    
+    def stop(self):
+        self.mouseListener.stop()
+        self.keyboardListener.stop()
+        self.running = False
+    
+    def __initListeners(self):
+        self.mouseListener = mouse.Listener(on_click=self.handler.handleMouseClick, 
+                                            on_move=self.handler.handleMouseMove, 
+                                            on_scroll=self.handler.handleMouseScroll)
+        
+        self.keyboardListener = keyboard.Listener(on_press=self.handler.handleKeyPress,
+                                               on_release=self.handler.handleKeyRelease)
