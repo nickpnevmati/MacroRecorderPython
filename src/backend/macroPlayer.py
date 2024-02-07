aad5f272f9057cdcf8a258fa640b431d0d2ba2ca
NickPnevmati <nickpnevmati@duck.com>
2024-02-07 23:01:04 +0200

    Implement macroPlayer skeleton class

--- src/backend/macroPlayer.py
+++ src/backend/macroPlayer.py
@@ -5,21 +5,10 @@ class MacroPlayer():
     def __init__(self) -> None:
         self.mouse = mouseController()
         self.keyboard = keyboardController()
+        # TODO load actions so we have them ready for execution
 
-        # # Move the self.mouse to a position on the screen
-        # self.mouse.position = (100, 150)
+    def start(self) -> None:
+        pass
 
-        # # Click the left self.mouse button
-        # self.mouse.click(Button.left, 1)
-
-        # # Type out a string
-        # self.keyboard.type('Hello, world!')
-
-        # # Press and release a single key
-        # self.keyboard.press(Key.enter)
-        # self.keyboard.release(Key.enter)
-
-        # # Perform a combination of key presses
-        # with self.keyboard.pressed(Key.ctrl):
-        #     self.keyboard.press('c')
-        #     self.keyboard.release('c')
+    def stop(self) -> None:
+        pass
