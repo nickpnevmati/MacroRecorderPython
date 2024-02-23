c15c05ccc21c70dc2e3a233d08a12f9bc6abff13
Nick Pnevmati <nickpnevmati@gmail.com>
2024-02-23 22:33:32 +0200

    reworked ui pipeline

--- main.py
+++ main.py
@@ -1,14 +1,36 @@
 # from src.MainWindow import MainWindow
 from PyQt5.QtWidgets import QApplication
-from src.mainWindow import MainWindow
-from src.logger import logger
+from src.frontend.mainwindow import Ui_MainWindow
 import sys
+from PyQt5 import QtWidgets
+# from src.frontend.TrayIcon import IconThread
+# from pystray import Icon, MenuItem
+# from PIL import Image
+
+class Window(QtWidgets.QMainWindow):
+    def __init__(self):
+        super(Window, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        # self.__createTrayIcon()
+        
+    # def __createTrayIcon(self):
+    #     menu = (MenuItem("Show", self.__TrayShow), MenuItem("Exit", self.__TrayExit))
+    #     image = Image.open("resources/logo512.png")
+    #     icon = IconThread(name="Macro Recorder Python", icon=image, menu=menu)
+    #     icon.start()
+
+    # def __TrayShow(self, icon, item):
+    #     self.show()
+
+    # def __TrayExit(self, icon, item):
+    #     self.app.quit()
 
 def main():
-	logger.info('App start')
 	app = QApplication(sys.argv)
-	w = MainWindow(app)
-	app.exec()
+	win = Window()
+	win.show()
+	sys.exit(app.exec_())
 
 if __name__ == '__main__':
    	main()
