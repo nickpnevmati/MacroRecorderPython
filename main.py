828c9b5dc70608da293a5a46a5fe4e4a09325694
Nick Pnevmati <nickpnevmati@gmail.com>
2024-02-23 22:37:04 +0200

    re-introduced tray icon code

--- main.py
+++ main.py
@@ -3,28 +3,28 @@ from PyQt5.QtWidgets import QApplication
 from src.frontend.mainwindow import Ui_MainWindow
 import sys
 from PyQt5 import QtWidgets
-# from src.frontend.TrayIcon import IconThread
-# from pystray import Icon, MenuItem
-# from PIL import Image
+from src.frontend.TrayIcon import IconThread
+from pystray import Icon, MenuItem
+from PIL import Image
 
 class Window(QtWidgets.QMainWindow):
     def __init__(self):
         super(Window, self).__init__()
         self.ui = Ui_MainWindow()
         self.ui.setupUi(self)
-        # self.__createTrayIcon()
+        self.__createTrayIcon()
         
-    # def __createTrayIcon(self):
-    #     menu = (MenuItem("Show", self.__TrayShow), MenuItem("Exit", self.__TrayExit))
-    #     image = Image.open("resources/logo512.png")
-    #     icon = IconThread(name="Macro Recorder Python", icon=image, menu=menu)
-    #     icon.start()
+    def __createTrayIcon(self):
+        menu = (MenuItem("Show", self.__TrayShow), MenuItem("Exit", self.__TrayExit))
+        image = Image.open("resources/logo512.png")
+        icon = IconThread(name="Macro Recorder Python", icon=image, menu=menu)
+        icon.start()
 
-    # def __TrayShow(self, icon, item):
-    #     self.show()
+    def __TrayShow(self, icon, item):
+        self.show()
 
-    # def __TrayExit(self, icon, item):
-    #     self.app.quit()
+    def __TrayExit(self, icon, item):
+        self.app.quit()
 
 def main():
 	app = QApplication(sys.argv)
