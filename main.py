b849a4a903de635bfe4248eece28e8275f951add
Nick Pnevmati <nickpnevmati@gmail.com>
2024-03-01 19:11:24 +0200

    changed over to Qt's tray method and it works flawlesly

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import sys
 import faulthandler
 from PyQt5.QtWidgets import QApplication
+from src.frontend.trayIcon import TrayIcon
 from src.frontend.mainWindow import MainWindow
 
 def main():
     faulthandler.enable()
-    # TODO start icon thread here
     app = QApplication(sys.argv)
+    app.setQuitOnLastWindowClosed(False)
     win = MainWindow()
+    
+    _ = TrayIcon(app, win) # Must keep reference here otherwise it gets garbage collected :) 
     win.show()
     app.exec()
     
--- src/frontend/trayIcon.py
+++ src/frontend/trayIcon.py
@@ -1,10 +1,23 @@
-import platform
-import threading
-from pystray import Icon
+from PyQt5.QtWidgets import QApplication, QSystemTrayIcon, QMenu, QAction
+from PyQt5.QtGui import QIcon
+from src.frontend.mainWindow import MainWindow
 
-class IconThread():
-    def __init__(self):
-        if platform.uname().system == 'Linux':
-            pass #TODO
-        elif platform.uname().system == 'Windows':
-            pass #TODO
+class TrayIcon():
+    def __init__(self, app: QApplication, win: MainWindow):
+        self.icon = QIcon('resources/logo512.png')
+        self.tray = QSystemTrayIcon()
+        self.menu = QMenu()
+        
+        self.tray.setIcon(self.icon)
+        self.tray.setVisible(True)
+        
+        self.exitAction = QAction("Exit")
+        self.showAction = QAction("Show")
+        
+        self.exitAction.triggered.connect(app.quit)
+        self.showAction.triggered.connect(win.show)
+        
+        self.menu.addAction(self.exitAction)
+        self.menu.addAction(self.showAction)
+        
+        self.tray.setContextMenu(self.menu)
