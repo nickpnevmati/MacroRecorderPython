5aa5a92fe69e6c91d759df054d547b392d557e6a
Nick Pnevmati <nickpnevmati@gmail.com>
2024-03-08 22:18:48 +0200

    implemented single instance "assurance"

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
+import atexit
+import os
+from pathlib import Path
 import sys
 import faulthandler
 from PyQt5.QtWidgets import QApplication
 from src.frontend.trayIcon import TrayIcon
 from src.frontend.mainWindow import MainWindow
 from src.connectionLayer import ConnectionLayer
+import src
+
+def ensureSingleInstance():
+    """
+    Ensured the existance of only one instance of the app using a file as a "lock"
+    """
+    lockFile = src.app_data_path / '~instance.lock'
+    if Path.exists(lockFile):
+        exit(1)
+    Path.touch(lockFile)
+    atexit.register(lambda : os.remove(lockFile))
 
 def main():
     faulthandler.enable()
+    ensureSingleInstance()
     app = QApplication(sys.argv)
     app.setQuitOnLastWindowClosed(False)
     win = MainWindow()
