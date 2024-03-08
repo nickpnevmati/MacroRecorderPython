9482521c6336dcbef920184620a0741f8b45083f
Nick Pnevmati <nickpnevmati@gmail.com>
2024-03-08 20:24:59 +0200

    Kill me

--- src/frontend/mainWindow.py
+++ src/frontend/mainWindow.py
@@ -1,5 +1,4 @@
 from PyQt5 import QtWidgets
-from src.connectionLayer import ConnectionLayer
 from src.frontend.ui.uimainwindow import Ui_MainWindow
 
 class MainWindow(QtWidgets.QMainWindow):
@@ -9,3 +8,35 @@ class MainWindow(QtWidgets.QMainWindow):
         self.ui.setupUi(self)
         
         self.connectionLayer = ConnectionLayer(self)
+        
+
+import json
+from src import user_data_dir
+from src.backend.macroManager import MacroManager
+
+class ConnectionLayer():
+    def __init__(self, window: MainWindow) -> None:
+        self.manager = MacroManager()
+        self.window = window
+        
+        self.__setup()
+        
+    def __setup(self):
+        """
+        This is where the UI layer is connected to the connlayer
+        """
+        self.window.ui.startRecordingButton.clicked.connect(self.__onStartRecording)
+        
+    def __onStartRecording(self):
+        self.window.hide()
+        self.manager.startRecording(self.__onStopRecording)
+        
+    def __onStopRecording(self):
+        self.window.show()
+        actions = self.manager.getActions()
+        self.__writeToFile('macros/tmp.macro', actions)
+        
+    def __writeToFile(self, filename: str, data):
+        path = user_data_dir() / filename
+        fp = open(path, 'w')
+        json.dump(data, fp)
